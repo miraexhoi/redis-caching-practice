38937f626bf7a494f9d5c9dc69f49cd8a249f027
최미래 <miraechoi@dgsw.hs.kr>
2024-07-26 20:37:16 +0900

    feat :: create member entity

---
+++ src/main/java/com/example/rediscachespringjava/domain/Member.java
@@ -0,0 +1,22 @@
+package com.example.rediscachespringjava.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+}
